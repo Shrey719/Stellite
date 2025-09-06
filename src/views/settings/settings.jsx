cf9f5142ba247bcaeb943e25070314f7f1de2280
Shrey719 <shrey150436@gmail.com>
2025-09-05 22:41:58 -0400

    fix settings CSS + make c u r v y

--- src/views/settings/settings.jsx
+++ src/views/settings/settings.jsx
@@ -1,12 +1,13 @@
 import styles from "../../App.module.css";
 import ProxySettings from "./categories/proxy";
 import Sidebar from "./sidebar.jsx";
+import settings from "./settings.module.css"
 
 function Settings() {
   return (
     <div class={styles.body}>
       <Sidebar />
-      <div id="settingsContent">
+      <div id="settingsContent" class={settings.content}>
         <ProxySettings />
       </div>
     </div>
