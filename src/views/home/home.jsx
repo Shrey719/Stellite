7baea37c5549714dac992081368df960e8af520f
Shrey719 <shrey150436@gmail.com>
2026-01-04 19:58:50 -0500

    ads but actually good.,..

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -19,6 +19,23 @@ function Home() {
   let httpslessdomain = /^(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?:\/.*)?$/;
 
   return (
+    <>
+    <div id="ads">
+<div style="position: absolute; z-index: 99999">
+      <input autocomplete="off" type="checkbox" id="aadsstickymk0g43gy" hidden />
+      <div style="padding-top: 0; padding-bottom: 0;">
+        <div style="width:120px;height:60px;position:fixed;text-align:center;font-size:0;top:50%;transform:translateY(-50%);right:0">
+          <label for="aadsstickymk0g43gy" class={styles.hideAd} style="top: -24px; left: 0;; position: absolute;border-radius: 4px; background: rgba(248, 248, 249, 0.70); padding: 4px;z-index: 99999;cursor:pointer">
+            <svg fill="#000000" height="16px" width="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490">
+              <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "/>
+            </svg>
+          </label>
+          <div id="frame" style="width: 120px;margin: auto;z-index: 99998;height: auto display: flex;flex-direction: column; justify-content: center">
+                        <iframe data-aa="2423216" src="//ad.a-ads.com/2423216/?size=120x60" style='border:0; padding:0; width:120px; height:60px; overflow:hidden; margin: 0 auto'></iframe>
+                    </div>
+        </div>
+    </div></div>
+    </div>
     <div id="mainBody">
       <div class={styles.glassContainer}>
         <div class={styles.body}>
@@ -46,6 +63,7 @@ function Home() {
         </div>
       </div>
     </div>
+    </>
   );
 }
 
